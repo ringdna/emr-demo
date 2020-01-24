117c75f0027c4488c3ef8d64a4f671b637c1e85e
Pete Wright <peter.wright@ringdna.com>
2020-01-23 17:21:56 -0800

    initial setup for airflow qa vpc to figure out mechanics

--- cluster.py
+++ cluster.py
@@ -2,13 +2,13 @@
 import boto3    
 
 # Variables configured via the infrastructure team.
-AWS_REGION='us-west-1'
-EMR_BUCKET='emr-airflow'
-EC2_KEYNAME="emr-dev"
+AWS_REGION='us-east-1'
+EMR_BUCKET='ringdna-emr-qa'
+EC2_KEYNAME="qa-20191001"
 BOOTSTRAP_URI = 's3://{}/scripts/bootstrap.sh'.format(EMR_BUCKET)
-EMR_MANAGED_MASTER_SECURITY_GROUP = "sg-0b7aa2ddd46472f34"
-EMR_MANAGED_SLAVE_SECURITY_GROUP = "sg-0b7aa2ddd46472f34"
-SUBNET_ID = "subnet-0e0c9ef325662c67d"
+EMR_MANAGED_MASTER_SECURITY_GROUP = "sg-e9291c80"
+EMR_MANAGED_SLAVE_SECURITY_GROUP = "sg-e9291c80"
+SUBNET_ID = "subnet-0734604af9d2cced2"
 
 if __name__ == '__main__':
     client = boto3.client('emr', region_name=AWS_REGION)
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     response = client.run_job_flow(
         Name="test_emr", 
         LogUri='s3://{}/logs/'.format(EMR_BUCKET), 
-        ReleaseLabel="emr-5.28.0", 
+        ReleaseLabel="emr-5.29.0",
         Applications=[
             {
                 "Name": "Hadoop"
